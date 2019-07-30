a06adb3f259eda456826106f085a53e881978434
Defolto <37343435+Defolto@users.noreply.github.com>
2019-07-30 12:37:30 +0300

    recent amendments

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ if (height < 810) {
 if (height < 1500) {
     $('#row1 > .col-md-3').eq(3).detach();
     $('#row1 > .col-md-3').eq(2).detach();
-    $('#row1 > .col-md-3').eq(1).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row1 > .col-md-3').eq(0).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row1').eq(0).addClass('d-flex justify-content-between');
+    $('#row1 > .col-md-3').eq(1).addClass('col-md-5').removeClass('col-md-3');
+    $('#row1 > .col-md-3').eq(0).addClass('col-md-5').removeClass('col-md-3');
+    $('#row1').eq(0).addClass('d-flex justify-content-around');
     $('#row2 > .col-md-3').eq(3).detach();
     $('#row2 > .col-md-3').eq(2).detach();
-    $('#row2 > .col-md-3').eq(1).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row2 > .col-md-3').eq(0).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row2').eq(0).addClass('d-flex justify-content-between');
+    $('#row2 > .col-md-3').eq(1).addClass('col-md-5').removeClass('col-md-3');
+    $('#row2 > .col-md-3').eq(0).addClass('col-md-5').removeClass('col-md-3');
+    $('#row2').eq(0).addClass('d-flex justify-content-around');
     $('#row3 > .col-md-3').eq(3).detach();
     $('#row3 > .col-md-3').eq(2).detach();
-    $('#row3 > .col-md-3').eq(1).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row3 > .col-md-3').eq(0).addClass('.col-md-4').removeClass('col-md-3');
-    $('#row3').eq(0).addClass('d-flex justify-content-between');
+    $('#row3 > .col-md-3').eq(1).addClass('col-md-5').removeClass('col-md-3');
+    $('#row3 > .col-md-3').eq(0).addClass('col-md-5').removeClass('col-md-3');
+    $('#row3').eq(0).addClass('d-flex justify-content-around');
 }
 if (height < 769) {
     $('#row2').detach();
